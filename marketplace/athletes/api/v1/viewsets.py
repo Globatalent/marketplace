a5338827bce730452959c37d4cf3f3e6d4cbda55
emsi <carmen@dekalabs.com>
2018-05-29 12:36:08 +0200

    add or delete a suppoerter as a follower

--- marketplace/athletes/api/v1/viewsets.py
+++ marketplace/athletes/api/v1/viewsets.py
@@ -1,6 +1,11 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from marketplace.athletes.api.v1.serializers import PictureSerializer, LinkSerializer, AthleteSerializer
 from marketplace.athletes.models import Picture, Link, Athlete
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.decorators import action
+
+from marketplace.supporters.models import Supporter
 
 
 class PictureViewSet(ReadOnlyModelViewSet):
@@ -14,5 +19,20 @@ class LinkViewSet(ReadOnlyModelViewSet):
 
 
 class AthleteViewSet(ReadOnlyModelViewSet):
-    serializer_class = AthleteSerializer
     queryset = Athlete.objects.all()
+    serializer_class = AthleteSerializer
+
+    @action(methods=['post'], detail=True)
+    def following(self, request, pk):
+        if not (hasattr(request.user, 'supporter')):
+            raise PermissionDenied
+
+        athlete = Athlete.objects.get(id=pk)
+        supporter = Supporter.objects.get(id=request.user.supporter.id)
+
+        if Supporter.objects.filter(following__id=pk).count() == 1:
+            supporter.following.remove(athlete)
+            return Response(data={'following': False})
+
+        supporter.following.add(athlete)
+        return Response(data={'following': True})
