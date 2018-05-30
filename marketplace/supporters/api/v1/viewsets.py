fefd79b7356639ed5eb88bc78c18fdfda3ea1cd6
emsi <carmen@dekalabs.com>
2018-05-30 12:48:58 +0200

    supporter can update his/her alerts

--- marketplace/supporters/api/v1/viewsets.py
+++ marketplace/supporters/api/v1/viewsets.py
@@ -1,12 +1,15 @@
+from django.views.generic import DetailView
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.mixins import CreateModelMixin
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.mixins import CreateModelMixin, UpdateModelMixin
+from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet, ViewSet, GenericViewSet
 from marketplace.supporters.models import Alert
 from marketplace.supporters.api.v1.serializers import AlertSerializer
 
 
 class AlertViewSet(CreateModelMixin,
-                   ReadOnlyModelViewSet):
+                   UpdateModelMixin,
+                   ReadOnlyModelViewSet,
+                   DetailView):
     serializer_class = AlertSerializer
     queryset = Alert.objects.all()
 
@@ -20,3 +23,15 @@ class AlertViewSet(CreateModelMixin,
 
     def get_queryset(self):
         return Alert.objects.filter(supporter__exact=self.request.user.supporter.id)
+
+    def update(self, request, *args, **kwargs):
+        if not (hasattr(request.user, 'supporter')):
+            raise PermissionDenied
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=False)
+        serializer.is_valid(raise_exception=True)
+
+        instance.rule = serializer.validated_data['rule']
+        instance.amount = serializer.validated_data['amount']
+
+        return super().update(request=request)
