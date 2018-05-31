6bd53ee960abe140c15d8d893de33cd9c5b51f89
emsi <carmen@dekalabs.com>
2018-05-31 12:04:55 +0200

    update read/not read  notification

--- marketplace/actions/api/v1/viewsets.py
+++ marketplace/actions/api/v1/viewsets.py
@@ -1,13 +1,14 @@
+from rest_framework.mixins import UpdateModelMixin
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from marketplace.actions.api.v1.serializers import NotificationSerializer
 from marketplace.actions.models import Notification
 
 
-class NotificationViewSet(ReadOnlyModelViewSet):
+class NotificationViewSet(UpdateModelMixin,
+                          ReadOnlyModelViewSet):
     serializer_class = NotificationSerializer
     queryset = Notification.objects.all()
 
     def get_queryset(self):
-        return Notification.objects.filter(user_id__exact=self.request.user.id)
-
+        return Notification.objects.filter(user_id=self.request.user.id)
