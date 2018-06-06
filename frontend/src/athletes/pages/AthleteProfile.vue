7e0ba0028eec443be7faf453b219ef7f801b0c90
Victor Borràs <victor@dekalabs.com>
2018-06-06 12:20:53 +0200

    add athlete profile page

---
+++ frontend/src/athletes/pages/AthleteProfile.vue
@@ -0,0 +1,27 @@
+<template>
+    <gb-base-layout>
+      Athlete Profile Page
+      <!--
+      TODO @kike: Here the athlete must be able to upload their data and add pictures and links (to videos, social networks, etc)
+      A picture has only the image. We can send them one by one or in bulk
+      A link has a name and a url, and they can add as much as they want
+
+      They must also be able to edit the data they submitted in the registration form (except the email for now)
+      -->
+    </gb-base-layout>
+</template>
+
+<script>
+  import BaseLayout from '@/layout/BaseLayout.vue'
+
+  export default {
+    name: 'AthleteProfile',
+    components: {
+      'gb-base-layout': BaseLayout
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,17 +1,26 @@
 import Login from '@/auth/pages/Login.vue'
 import Registration from '@/auth/pages/Registration.vue'
+import AthleteProfile from '@/athletes/pages/AthleteProfile.vue'
 
 export default [
   {
     path: '/login',
-    name: 'Login',
+    name: 'login',
     component: Login
   },
   {
     path: '/register',
-    name: 'Registration',
+    name: 'registration',
     component: Registration
   },
+
+  // Athletes
+  {
+    path: '/athlete-profile',
+    name: 'athlete.profile',
+    component: AthleteProfile
+  },
+
   // 404
   {
     path: '/404',
