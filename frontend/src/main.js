40effaf38d7e5a5103615c1cca677d4c7c03575e
Victor Borràs <victor@dekalabs.com>
2018-06-06 10:50:00 +0200

    load element styles

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router'
 import { store } from './store/index'
 import VueMoment from 'vue-moment'
-import './scss/reset.css'
+import './scss/theme.scss'
 
 Vue.config.productionTip = false
 
