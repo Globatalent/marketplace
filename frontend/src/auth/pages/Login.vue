1525743432c921f0588612397426e7f8b6bb00e0
Enrique Querol <enrique.eqg@gmail.com>
2018-06-07 11:08:52 +0200

    Login responsive styles
    Font files added.

--- frontend/src/auth/pages/Login.vue
+++ frontend/src/auth/pages/Login.vue
@@ -9,19 +9,32 @@
 
      It should be centered (horizontally and vertically) and not be too wide
     -->
-    <el-row>
-      <el-col :span="12" :offset="6">
+    <div class="is-vertical-center">
+    <el-row type="flex" justify="center">
+      <el-col :xs="24" :sm="12" :md="12" class="loginBox text-center">
+        <img class="logo" src="~@/assets/img/Globatalent-logo-vert.png" />
         <h2>Sign In</h2>
         <el-form ref="form">
           <el-form-item prop="email" required>
             <el-input placeholder="Email" type="email"></el-input>
           </el-form-item>
           <el-form-item prop="password" required>
-            <el-input placeholder="Password" type="password" auto-complete="off"></el-input>
+            <el-input placeholder="Password" type="password"></el-input>
+          </el-form-item>
+          <el-form-item class="text-left">
+            <el-checkbox label="Remember me"></el-checkbox>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" class="is-uppercase">Log in</el-button>
+          </el-form-item>
+          <el-form-item>
+            <a href=""><i class="el-icon-setting"></i> Forgot your password?</a>
           </el-form-item>
         </el-form>
+        <div>Dont't have an account? <a href="" class="is-main-color">Sign Up</a></div>
       </el-col>
     </el-row>
+    </div>
   </gb-minimal-layout>
 </template>
 
@@ -36,5 +49,11 @@
   }
 </script>
 
-<style scoped>
+<style type="scss" scoped>
+  .logo {
+    display: block;
+    margin: 0 auto 20px auto;
+  }
+  .loginBox{
+  }
 </style>
