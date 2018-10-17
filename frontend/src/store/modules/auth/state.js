ef3c2004cb41c45b2a51d1b9983f87bdc7372ce1
Marcos Gabarda <hey@marcosgabarda.com>
2018-10-17 10:54:59 +0200

    fixed register

--- frontend/src/store/modules/auth/state.js
+++ frontend/src/store/modules/auth/state.js
@@ -4,6 +4,7 @@ export default {
     obtainJWT: '/api/v1/auth/jwt/token/',
     refreshJWT: '/api/v1/auth/jwt/refresh/',
     signUp: '/api/v1/register/',
+    users: '/api/v1/users/',
     requestRestoreCode: '/api/v1/request_restore_code/',
     restorePassword: '/api/v1/restore_password/',
     registerAthlete: '/api/v1/register/athletes/',
