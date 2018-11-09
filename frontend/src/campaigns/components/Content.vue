2f8d045ec560f160fa370a9ba9189d27caf1ef5d
Victor Borràs <victor@dekalabs.com>
2018-11-09 19:08:48 +0100

    finish image upload component and apply it to campaign creation

--- frontend/src/campaigns/components/Content.vue
+++ frontend/src/campaigns/components/Content.vue
@@ -6,12 +6,17 @@
       <el-breadcrumb-item><a href="/campaigns/create">Content</a></el-breadcrumb-item>
     </el-breadcrumb>
     <div class="formSteps-actions">
-      <el-button v-if="campaign.isDraft" type="danger" class="" @click.prevent="onDiscard()">{{ $tc("message.DiscardCampaign") }}</el-button>
-      <el-button type="secondary" class="" @click.prevent="onSaveAndContinue()">{{ $tc("message.ReviewLaunch") }}</el-button>
+      <el-button v-if="campaign.isDraft" type="danger" class="" @click.prevent="onDiscard()">{{
+        $tc('message.DiscardCampaign') }}
+      </el-button>
+      <el-button type="secondary" class="" @click.prevent="onSaveAndContinue()">{{ $tc('message.ReviewLaunch') }}
+      </el-button>
     </div>
     <div class="formSteps">
-      <h2 class="formSteps-title">{{ $tc("message.Content") }}</h2>
-      <p class="formSteps-text">Make a good first impression: introduce your campaign objectives and entice people to learn more. This basic information will represent your campaign on your campaign page, on your campaign card, and in searches.</p>
+      <h2 class="formSteps-title">{{ $tc('message.Content') }}</h2>
+      <p class="formSteps-text">Make a good first impression: introduce your campaign objectives and entice people to
+        learn more. This basic information will represent your campaign on your campaign page, on your campaign card,
+        and in searches.</p>
       <el-form-item :label="$tc('message.SocialMedia')">
         <div v-for="(link, index) in form.links" :key="index" class="socialLinks">
           <span :class="link.network+' socialLinks-icon'"></span>
@@ -29,7 +34,8 @@
         <el-input type="text" v-model="form.coach"></el-input>
       </el-form-item>
       <el-form-item required :label="$tc('message.PitchVideoorImage')">
-        <p class="formSteps-inputText">Add a video or image to appear on the top of your campaign page. Campaigns with videos
+        <p class="formSteps-inputText">Add a video or image to appear on the top of your campaign page. Campaigns with
+          videos
           raise 2000% more then campaigns without videos. Keep your video 2-3 minutes. </p>
         <el-tabs v-model="activePitch" @tab-click="handleClick">
           <el-tab-pane label="Add Video" name="video">
@@ -39,127 +45,136 @@
           </el-tab-pane>
           <el-tab-pane label="Add Image" name="image">
             <div class="tabTitle">Add Image</div>
-            <el-upload drag thumbnail-mode :limit="1" :action="options.action" :headers="options.headers" :name="options.name" :http-request="options.httpRequest" :file-list="!!form.pitchImage ? [{name: form.pitchImage.substring(form.pitchImage.lastIndexOf('/') + 1), url: form.pitchImage}] : []">
-              <i class="el-icon-upload" style="padding: 0"></i>
-              <div class="el-upload__text">Upload Image</div>
-            </el-upload>
+
+            <gb-image-upload field-name="pitch_image" :campaign-id="campaign.id" v-if="campaign"
+                             :image-url="form.pitchImage" @image-changed="updateImage('pitchImage', $event)">
+            </gb-image-upload>
           </el-tab-pane>
         </el-tabs>
       </el-form-item>
       <el-form-item>
         <div class="formSteps-lineButton"></div>
-        <el-button type="primary" class="is-uppercase" @click.prevent="onSaveAndContinue()">{{ $tc("message.SaveContinue") }}</el-button>
+        <el-button type="primary" class="is-uppercase" @click.prevent="onSaveAndContinue()">{{
+          $tc('message.SaveContinue') }}
+        </el-button>
       </el-form-item>
     </div>
   </el-col>
 </template>
 
 <script>
-import Vue from 'vue'
-import { mapGetters } from 'vuex'
-import router from '@/router.js'
-import ajax from '@/base/helpers/ajax'
+  import Vue from 'vue'
+  import { mapGetters } from 'vuex'
+  import router from '@/router.js'
+  import ajax from '@/base/helpers/ajax'
+  import ImageUpload from '@/campaigns/components/ImageUpload.vue'
 
-export default {
-  name: 'Content',
-  components: {},
-  data() {
-    return {
-      // Form
-      form: {
-        links: [],
-        height: null,
-        weight: null,
-        club: null,
-        coach: null,
-        pitchUrl: null,
-        pitchImage: null
-      },
-      // Card Image
-      options: {
-        name: 'pitch_image',
-        headers: {
-          Authorization: this.$store.getters['auth/header']
+  export default {
+    name: 'Content',
+    components: {
+      'gb-image-upload': ImageUpload
+    },
+    data () {
+      return {
+        // Form
+        form: {
+          links: [],
+          height: null,
+          weight: null,
+          club: null,
+          coach: null,
+          pitchUrl: null,
+          pitchImage: null
         },
-        httpRequest: ajax,
-        action: ''
-      },
-      activePitch: 'video'
-    }
-  },
-  computed: {
-    ...mapGetters({
-      campaign: 'campaigns/campaign',
-      user: 'users/user'
-    })
-  },
-  created() {
-    this.$store
-      .dispatch('campaigns/fetch', this.$route.params.campaignId)
-      .then(() => {
-        if (!!this.campaign && !!this.campaign.id) {
-          this.options.action = `${
-            Vue.axios.defaults.baseURL
-          }/api/v1/campaigns/${this.campaign.id}/`
-          this.form = { ...this.campaign }
-          this.form.links = this.initialSocialLinks(this.campaign)
-        }
-      }).catch(()=> router.push({name: 'campaign.create'}))
-  },
-  methods: {
-    handleClick(tab, event) {
-      // console.log(tab, event)
-    },
-    initialSocialLinks(campaign) {
-      const networks = [
-        'facebook',
-        'instagram',
-        'twitter',
-        'youtube',
-        'linkedin',
-        'whatsapp',
-        'flickr',
-        'web',
-      ]
-      const socialLinks = networks.map(network => {
-        const links = campaign.links.filter(item => item.network == network)
-        if (links.length > 0) {
-          return links[0]
-        }
-        return { id: null, campaign: campaign.id, network: network, url: null }
-      })
-      return socialLinks
-    },
-    onDiscard() {
-      const payload = { id: this.campaign.id }
-      this.$store.dispatch('campaigns/delete', payload).then( () => {
-        router.push({name: 'campaign.create'})
-      })
-    },
-    onSaveAndContinue() {
-      const payload = {
-        id: this.campaign.id,
-        links: this.form.links,
-        height: this.form.height,
-        weight: this.form.weight,
-        club: this.form.club,
-        coach: this.form.coach,
-        pitchUrl: this.form.pitchUrl,
-        pitchImage: this.form.pitchImage
+        // Card Image
+        options: {
+          name: 'pitch_image',
+          headers: {
+            Authorization: this.$store.getters['auth/header']
+          },
+          httpRequest: ajax,
+          action: ''
+        },
+        activePitch: 'video'
       }
-      this.$store.dispatch('campaigns/update', payload).then(() => {
-        this.form = { ...this.campaign }
-        router.push({
-          name: 'campaign.edit',
-          params: {
-            campaignId: this.campaign.id,
-            step: 'career'
-          }
-        })
+    },
+    computed: {
+      ...mapGetters({
+        campaign: 'campaigns/campaign',
+        user: 'users/user'
       })
+    },
+    created () {
+      this.$store
+        .dispatch('campaigns/fetch', this.$route.params.campaignId)
+        .then(() => {
+          if (!!this.campaign && !!this.campaign.id) {
+            this.options.action = `${
+              Vue.axios.defaults.baseURL
+              }/api/v1/campaigns/${this.campaign.id}/`
+            this.form = {...this.campaign}
+            this.form.links = this.initialSocialLinks(this.campaign)
+          }
+        }).catch(() => router.push({name: 'campaign.create'}))
+    },
+    methods: {
+      handleClick (tab, event) {
+        // console.log(tab, event)
+      },
+      initialSocialLinks (campaign) {
+        const networks = [
+          'facebook',
+          'instagram',
+          'twitter',
+          'youtube',
+          'linkedin',
+          'whatsapp',
+          'flickr',
+          'web',
+        ]
+        const socialLinks = networks.map(network => {
+          const links = campaign.links.filter(item => item.network == network)
+          if (links.length > 0) {
+            return links[0]
+          }
+          return {id: null, campaign: campaign.id, network: network, url: null}
+        })
+        return socialLinks
+      },
+      onDiscard () {
+        const payload = {id: this.campaign.id}
+        this.$store.dispatch('campaigns/delete', payload).then(() => {
+          router.push({name: 'campaign.create'})
+        })
+      },
+      updateImage (fieldName, newURL) {
+        debugger
+        this.form[fieldName] = newURL
+      },
+      onSaveAndContinue () {
+        const payload = {
+          id: this.campaign.id,
+          links: this.form.links,
+          height: this.form.height,
+          weight: this.form.weight,
+          club: this.form.club,
+          coach: this.form.coach,
+          pitchUrl: this.form.pitchUrl,
+          pitchImage: this.form.pitchImage
+        }
+        this.$store.dispatch('campaigns/update', payload).then(() => {
+          this.form = {...this.campaign}
+          router.push({
+            name: 'campaign.edit',
+            params: {
+              campaignId: this.campaign.id,
+              step: 'career'
+            }
+          })
+        })
+      }
     }
   }
-}
 </script>
 
 <style type="scss" scoped>
