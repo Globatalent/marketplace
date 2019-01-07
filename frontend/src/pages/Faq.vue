ed4464bc593c388d8a16fe3744d5f64490549c39
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-01-07 14:20:55 +0100

    Update Faq.vue

--- frontend/src/pages/Faq.vue
+++ frontend/src/pages/Faq.vue
@@ -1,17 +1,102 @@
 <template>
   <gb-base-layout>
     <el-row>
-      <el-col :xs="24">
+      <el-col :xs="24" :sm="{span: 12, offset: 6}">
         <div class="beginBlock text-center">
           <h3 class="beginBlock-title">{{ $tc("message.WeBringTheTalent") }}</h3>
-          <h4 class="beginBlock-subTitle">Faq</h4>
+          <h3 class="beginBlock-subTitle">Faq</h3>
         </div>
       </el-col>
     </el-row>
-
-    <el-row>
+<el-row>
       <el-col :xs="24" :sm="{span: 12, offset: 6}">
-        <p>Coming soon!</p>
+        <!-- <div class="beginBlock text-center"> -->
+          <h4 class="campaign-name" style="color:	#0375B2">What are the basics?</h4>
+          <p style="color: #647585">The first sports crypto exchange where you can tokenize, trade and support your favorite clubs and sports idols. Although contributing to a campaign is not the same as purchasing a product or betting, when you support a campaign, you help your favorite clubs and sports idols and be part of them participating in their benefits.</p>
+          <p style="color: #647585">Although contributing to a campaign is not the same as purchasing a product or betting, when you support a campaign, you help your favorite clubs and sports idols and be part of them participating in their benefits.</p>
+
+          <h4 class="campaign-name" style="color:	#0375B2">What is Globatalent?</h4>
+          <p style="color: #647585">
+            A <strong style="font-weight: bold">campaign</strong> is a project an athlete or a club presents to the community asking for support to make it a reality and participate the fans.
+          </p>
+          <p  style="color: #647585">
+            The <strong style="font-weight: bold">funding goal</strong> is the amount of money that a creator needs to complete their project.
+          </p>
+          <p style="color: #647585">
+            Funding on Globatalent is <strong style="font-weight: bold">all-or-nothing</strong>. No one will be charged for a pledge towards a project unless it reaches its funding goal. This way, creators always have the budget they scoped out before moving forward. 
+          </p>
+          <p style="color: #647585">
+            A <strong style="font-weight: bold">creator</strong> is the athlete or club behind the campaign, working to bring it to life.
+          </p>
+          <p style="color: #647585">
+            <strong style="font-weight: bold">Supporters</strong> are folks who <strong style="font-weight: bold">pledge</strong> money to join creators in bringing projects to life. Globatalent is not a store, backers support a creative process.
+          </p>
+          <p style="color: #647585">
+            <strong style="font-weight: bold">Rewards/Rights</strong> are a creator’s chance to share a piece of their project with their backer community. Typically, these are % of future incomes, one-of-a-kind experiences…
+          </p>
+          <p style="color: #647585">
+            If you want to know more about how it works, try browsing around and looking at some of the projects on the site — you can start here. If you’re an athlete or club and you are curious about starting a project, our Globatalent Deck can give you a great overview. Check out Our Rules for more information.
+          </p>
+          <h4 class="campaign-name" style="color:	#0375B2">How does Globatalent platform work?</h4>
+          <p style="color: #647585">
+            The Globatalent Marketplace Platform connects  athletes and clubs with aneed for funding with contributing crowds on a micro level (fans). 
+          </p>
+          <h4 class="campaign-name" style="color:	#0375B2">How does Globatalent exchange work?</h4>
+          <p style="color: #647585">
+            The Globatalent Sports Exchange is the place where you can buy and sell the tokens of your favorite athletes or clubs. It will be very easy to use, you can buy and operate with a credit card
+          </p>
+          <h4 class="campaign-name" style="color:	#0375B2">How to contribute to a campaign and buy tokens?</h4>
+          <ul>
+            <li>1. Register on Globatalent platform</li>
+            <li>2. Browse and discover the campaign you want to support.</li>
+            <li>3. Press the “buy tokens” button.</li>
+            <li>4. Enter the amount of tokens you want to buy.</li>
+          </ul>
+          <h4 class="campaign-name" style="color:	#0375B2">Can you buy tokens with your credit card?</h4>
+          <p style="color: #647585">
+            Currently you can buy with your credit card.
+          </p>
+
+          <h4 class="campaign-name" style="color:	#0375B2">Can I trade the tokens I bought?</h4>
+          <p style="color: #647585">
+            Absolutely! we have designed the Globatalent Sports Exchange, where you can trade your tokens, and buy others, and be part of the new chapter in sports business
+          </p>
+          <h4 class="campaign-name" style="color:	#0375B2">Who can start a campaign?</h4>
+          <p style="color: #647585">
+          To start a campaign you must be a club or an athlete that needs funding.
+          Everyone can send a campaign proposal to Globatalent, you just have to go to “Start a Campaign” and fill in all the information requested.
+          The Globatalent team will evaluate the proposal and will contact you very soon.
+          </p>
+          <h4 class="campaign-name" style="color:	#0375B2">Why do people support campaigns?</h4>
+          <p style="color: #647585">
+            Many supporters are rallying around their friends’ projects. Some are supporting a new effort from someone they’ve long admired. Some are just inspired by is favourite clubs or sports idol, while others are motivated to pledge by a campaign’s rewards — a copy of what’s being produced, a limited edition, or a custom experience related to the campaign.
+          </p>
+          <p style="color: #647585">
+            Supporting a project is more than just pledging funds to a creator and is unlike purchasing items from a shop. It’s pledging your support to an athlete or a club, that you want to see exist in the world.
+          </p>  
+          <h4 class="campaign-name" style="color:	#0375B2">Where do supporters come from?</h4>
+          <p style="color: #647585">
+            The majority of initial funding usually comes from the fans and friends of each project. If they like it, they’ll spread the word to their friends, and so on. 
+          </p>
+          <p style="color: #647585">
+            Press, blogs, Twitter, Facebook, and Globatalent itself are also big sources of traffic and pledges. Altogether, millions of people visit Globatalent marketplace every week.
+          </p>
+          <h4 class="campaign-name" style="color:	#0375B2">How do I ask the campaign owner a question?</h4>
+          <p style="color: #647585">
+            Campaigns are run by their individual Campaign Owners, not by Globatalent. As such, they are best able to help answer any questions you might have about their project or perks they are offering!
+          </p>
+          <p style="color: #647585">
+            If you have any questions or concerns about a particular campaign, or perks offered in a campaign, or claims made in a campaign, we recommend that you get in touch with the campaign owner directly. 
+          </p>
+          <p style="color: #647585">
+            If you have an Globatalent account and have contributed to the campaign, you can post a comment to the campaign page, or you can send a direct message to the campaign owner
+          </p>
+          <h4 class="campaign-name" style="color:	#0375B2">What happens to my money if a campaign fails?</h4>
+          <p style="color: #647585">
+            If the campaign you buy tokens for does not reach the soft cap defined, your money will be reimbursed. 
+          </p>
+
+        <!-- </div> -->
       </el-col>
     </el-row>
 
