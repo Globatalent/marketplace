c117410c709b0c3e28f4e0199c6e96181c04526b
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-01-26 04:37:00 +0100

    Update CampaignDetails.vue

--- frontend/src/campaigns/pages/CampaignDetails.vue
+++ frontend/src/campaigns/pages/CampaignDetails.vue
@@ -162,9 +162,9 @@
         <gb-campaign-info-row v-if="campaign.history" id="storySection" :title="$tc('message.History')">
           <pre>{{campaign.history}}</pre>
         </gb-campaign-info-row>
-        <gb-campaign-info-row v-if="campaign.pitchUrl" id="pitchUrlSection" :title="$tc('message.Video')">
-          <iframe v-if="campaign.pitchUrl.includes('youtube')" width="560" height="315" src="https://www.youtube.com/embed/kJ3sxh-bHJs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          <a v-else :href="campaign.pitchUrl" target="_blank">{{campaign.pitchUrl}}</a>
+        <gb-campaign-info-row v-if="campaign.pitchUrl" id="pitchUrlSection" :title="$tc('message.Video')"> https://www.youtube.com/embed/kJ3sxh-bHJs
+          <iframe v-if="campaign.pitchUrl.includes('youtube')" width="560" height="315" v-bind:src="'https://www.youtube.com/embed/' + campaign.pitchUrlsplit('?').slice(-1)[0].split('&').filter(param => param.includes('v=')).slice(-1)[0].split('v=').slice(-1)[0]" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe v-if="campaign.pitchUrl.includes('vimeo')" v-bind:src="'https://player.vimeo.com/video/' + campaign.pitchUrl.split('/').slice(-1)[0]" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
         </gb-campaign-info-row>
 
         <gb-campaign-info-row v-if="campaign.height" :title="$tc('message.Height')">
