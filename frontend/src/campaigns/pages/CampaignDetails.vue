4e3d1459107d0379184c8c4406fb8b6f861d0d4d
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-02-05 19:40:57 +0100

    Update CampaignDetails.vue

--- frontend/src/campaigns/pages/CampaignDetails.vue
+++ frontend/src/campaigns/pages/CampaignDetails.vue
@@ -267,7 +267,7 @@
         <v-tab title="Feeds">
           <div v-if="campaign.links.length > 0">
           <twitter>
-            <a class="twitter-timeline" data-link-color="#6aa5dc" data-dnt="true" data-tweet-limit="5" :href="campaign.link.filter(site => site.network == 'twitter')[0].url + '?ref_src=twsrc%5Etfw'"></a> 
+            <a class="twitter-timeline" data-link-color="#6aa5dc" data-dnt="true" data-tweet-limit="5" :href="campaign.links.filter(site => site.network == 'twitter')[0].url + '?ref_src=twsrc%5Etfw'"></a> 
           </twitter>
           </div>
         </v-tab>
