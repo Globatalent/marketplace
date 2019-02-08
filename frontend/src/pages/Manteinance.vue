cb33a77f1cf96fbc6744eedb3a8bffdf770cddae
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-02-08 08:55:40 +0100

    manteinance

---
+++ frontend/src/pages/Manteinance.vue
@@ -0,0 +1,110 @@
+<template>
+  <gb-base-layout>
+    <el-row>
+      <el-col :xs="24" :sm="{span: 12, offset: 6}">
+        <div class="beginBlock text-center">
+          <h3 class="beginBlock-title">{{ $tc("message.WeBringTheTalent") }}</h3>
+          <h3 class="beginBlock-subTitle">Faq</h3>
+        </div>
+      </el-col>
+    </el-row>
+<el-row>
+      <el-col :xs="24" :sm="{span: 12, offset: 6}">
+        <!-- <div class="beginBlock text-center"> -->
+          <!-- <h4 class="campaign-name" style="color:	#0375B2">{{$tc('message.whatAreTheBasicsTitle')}}</h4>
+          <div v-html="$t('message.whatAreTheBasics')"></div> -->
+          <h1>Globatalent is in scheduled manteinance mode. We are working hard to improve our site, and we will be back soon!!</h1>
+
+        <!-- </div> -->
+      </el-col>
+    </el-row>
+
+    <div class="campaignList-startBlock">
+      <div class="campaignList-startBlock-container">
+        <div class="campaignList-startBlock-sentence">
+          <router-link :to="{ name: 'campaign.create'}" class="is-main-color">
+            <el-button type="primary" class="startFreeButton" size="big" v-html="$tc('message.StartCampaignNow')"></el-button>
+          </router-link>
+        </div>
+        <!-- <el-button type="primary" class="startFreeButton" size="big">{{$tc('message.StartFreeTrial')}}</el-button> -->
+      </div>
+    </div>
+  </gb-base-layout>
+</template>
+
+<script>
+import BaseLayout from '@/layout/BaseLayout.vue'
+
+export default {
+  name: 'FaqPage',
+  components: {
+    'gb-base-layout': BaseLayout
+  },
+  data() {
+    return {}
+  },
+
+  created() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../scss/variables.scss';
+
+  .beginBlock {
+    background: url('../assets/img/background-list-title.png');
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+
+  .campaignList-startBlock {
+    margin: 30px auto;
+    border-top: 1px solid $--grey-detailCampaign-border;
+    padding-top: 40px;
+    display: block;
+    width: 100%;
+    max-width: 895px;
+    font-size: 0px;
+  }
+
+  .campaignList-startBlock-container {
+    max-width: 585px;
+    display: block;
+    margin: 0 auto;
+  }
+
+  .campaignList-startBlock-sentence {
+    display: inline-block;
+    vertical-align: top;
+    text-align: right;
+    padding-right: 20px;
+    padding-right: 0px;
+    width: 70%;
+    width: 100%;
+    text-align: center;
+  }
+
+  .campaignList-startBlock-sentence1 {
+    font-weight: normal;
+    font-family: 'Aller';
+    font-size: 36px;
+    line-height: 36px;
+    margin: 0;
+  }
+
+  .campaignList-startBlock-sentence2 {
+    font-size: 14px;
+  }
+
+  .startFreeButton {
+    display: inline-block;
+    vertical-align: top;
+    width: auto;
+    font-size: 14px;
+    font-family: 'Aller';
+    font-weight: bold;
+    position: relative;
+    top: 6px;
+  }
+</style>
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,29 +2,29 @@ export default [
   {
     path: '/login',
     name: 'login',
-    component: resolve => require(['@/auth/pages/Login.vue'], resolve),
+    component: resolve => require(['@/auth/pages/Login.vue'], resolve)
   },
   {
     path: '/register',
     name: 'registration',
-    component: resolve => require(['@/auth/pages/Registration.vue'], resolve),
+    component: resolve => require(['@/auth/pages/Registration.vue'], resolve)
   },
   {
     path: '/restore-password/:restorePasswordCode/',
     name: 'restore.password',
     component: resolve => require(['@/auth/pages/RestorePassword.vue'], resolve),
-    props: true,
+    props: true
   },
   {
     path: '/forgot',
     name: 'forgot',
-    component: resolve => require(['@/auth/pages/Forgot.vue'], resolve),
+    component: resolve => require(['@/auth/pages/Forgot.vue'], resolve)
   },
   {
     path: '/verified/:verificationCode/',
     name: 'verified',
     component: resolve => require(['@/auth/pages/Verified.vue'], resolve),
-    props: true,
+    props: true
   },
 
   // User
@@ -33,20 +33,20 @@ export default [
     name: 'profile',
     component: resolve => require(['@/users/pages/Profile.vue'], resolve),
     meta: {
-      auth: true,
-    },
+      auth: true
+    }
   },
   {
     path: '/notifications',
     name: 'notifications',
-    component: resolve => require(['@/actions/pages/Notifications.vue'], resolve),
+    component: resolve => require(['@/actions/pages/Notifications.vue'], resolve)
   },
 
   // Campaigns
   {
     path: '/campaigns',
     name: 'campaign.list',
-    component: resolve => require(['@/campaigns/pages/CampaignList.vue'], resolve),
+    component: resolve => require(['@/campaigns/pages/CampaignList.vue'], resolve)
   },
   {
     path: '/campaigns/create',
@@ -54,8 +54,8 @@ export default [
     component: resolve => require(['@/campaigns/pages/CampaignCreate.vue'], resolve),
     props: true,
     meta: {
-      auth: true,
-    },
+      auth: true
+    }
   },
   {
     path: '/campaigns/:campaignId/edit/:step',
@@ -63,8 +63,8 @@ export default [
     component: resolve => require(['@/campaigns/pages/CampaignEdit.vue'], resolve),
     props: true,
     meta: {
-      auth: true,
-    },
+      auth: true
+    }
   },
   {
     path: '/campaigns/:campaignId/success',
@@ -72,22 +72,22 @@ export default [
     component: resolve => require(['@/campaigns/pages/CampaignSuccess.vue'], resolve),
     props: true,
     meta: {
-      auth: true,
-    },
+      auth: true
+    }
   },
   {
     path: '/campaigns/:campaignId',
     name: 'campaign.details',
     component: resolve => require(['@/campaigns/pages/CampaignDetails.vue'], resolve),
-    props: true,
+    props: true
   },
   {
     path: '/purchases/:campaignId',
     name: 'campaign.invest',
     component: resolve => require(['@/campaigns/pages/CampaignInvest.vue'], resolve),
     meta: {
-      auth: true,
-    },
+      auth: true
+    }
   },
   {
     path: '/purchased/:purchaseId',
@@ -95,8 +95,8 @@ export default [
     component: resolve => require(['@/campaigns/pages/CampaignInvested.vue'], resolve),
     props: true,
     meta: {
-      auth: true,
-    },
+      auth: true
+    }
   },
 
   {
@@ -104,78 +104,88 @@ export default [
     name: 'purchase',
     component: resolve => require(['@/pages/Purchase.vue'], resolve),
     meta: {
-      auth: true,
-    },
+      auth: true
+    }
   },
   {
     path: '/sport',
     name: 'sport',
-    component: resolve => require(['@/pages/Sport.vue'], resolve),
+    component: resolve => require(['@/pages/Sport.vue'], resolve)
   },
   {
     path: '/faq',
     name: 'faq',
-    component: resolve => require(['@/pages/Faq.vue'], resolve),
+    component: resolve => require(['@/pages/Faq.vue'], resolve)
   },
   {
     path: '/terms',
     name: 'terms',
-    component: resolve => require(['@/pages/Terms.vue'], resolve),
+    component: resolve => require(['@/pages/Terms.vue'], resolve)
   },
   {
     path: '/risks',
     name: 'risks',
-    component: resolve => require(['@/pages/Risks.vue'], resolve),
+    component: resolve => require(['@/pages/Risks.vue'], resolve)
   },
   {
     path: '/news',
     name: 'news',
-    component: resolve => require(['@/pages/News.vue'], resolve),
+    component: resolve => require(['@/pages/News.vue'], resolve)
   },
   {
     path: '/about',
     name: 'about',
-    component: resolve => require(['@/pages/About.vue'], resolve),
+    component: resolve => require(['@/pages/About.vue'], resolve)
   },
   {
     path: '/contact',
     name: 'contact',
-    component: resolve => require(['@/pages/Contact.vue'], resolve),
+    component: resolve => require(['@/pages/Contact.vue'], resolve)
   },
   {
     path: '/athletesandorganizations',
     name: 'organizations',
-    component: resolve => require(['@/pages/Organizations.vue'], resolve),
+    component: resolve => require(['@/pages/Organizations.vue'], resolve)
   },
   {
     path: '/press',
     name: 'press',
-    component: resolve => require(['@/pages/Press.vue'], resolve),
+    component: resolve => require(['@/pages/Press.vue'], resolve)
   },
   {
     path: '/careers',
     name: 'careers',
-    component: resolve => require(['@/pages/Careers.vue'], resolve),
+    component: resolve => require(['@/pages/Careers.vue'], resolve)
   },
   {
     path: '/deck',
     name: 'deck',
-    component: resolve => require(['@/pages/Deck.vue'], resolve),
+    component: resolve => require(['@/pages/Deck.vue'], resolve)
   },
   // 404
   {
     path: '/404',
     name: 'not-found',
-    component: resolve => require(['@/pages/NotFound.vue'], resolve),
+    component: resolve => require(['@/pages/NotFound.vue'], resolve)
+  },
+  {
+    path: '/manteinance',
+    name: 'manteninance',
+    component: resolve => require(['@/pages/Manteinance.vue'], resolve)
   },
 
   // Redirects
+  // {
+  //   path: '/',
+  //   redirect: '/campaigns'
+  // },
   {
     path: '/',
-    redirect: '/campaigns',
+    redirect: '/manteinance'
   },
+
   {
     path: '/*',
-    redirect: '/404',
-  },
+    redirect: '/404'
+  }
 ]
