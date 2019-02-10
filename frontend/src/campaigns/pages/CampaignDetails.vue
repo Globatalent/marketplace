58012324bd191e1ff0a3a6c224522c2027d06aa8
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-02-10 13:10:56 +0100

    Update CampaignDetails.vue

--- frontend/src/campaigns/pages/CampaignDetails.vue
+++ frontend/src/campaigns/pages/CampaignDetails.vue
@@ -287,11 +287,9 @@
         </div>
           </v-tab>
           <v-tab title="Feeds">
-            <div v-if="campaign.links.length > 0">
             <twitter>
-              <a class="twitter-timeline" data-link-color="#6aa5dc" data-dnt="true" data-tweet-limit="5" :href="campaign.links.filter(site => site.network === 'twitter')[0].url.split('?')[0] + '?ref_src=twsrc%5Etfw'"></a> 
+              <a class="twitter-timeline" data-link-color="#6aa5dc" data-dnt="true" data-tweet-limit="5" :href="twitter_url"></a> 
             </twitter>
-            </div>
           </v-tab>
         </vue-tabs>
       </div>
@@ -403,11 +401,10 @@
         //   params: {campaignId: campaign.id}
         // })
       },
-      // getSocialUrl(links, network) {
-      //   if (links !== undefined) {
-      //     return links.filter(site => site.network === network)[0].url.split('?')[0] + '?ref_src=twsrc%5Etfw'
-      //   }
-      // }
+      getSocialUrl(links, network) {
+          // return links.filter(site => site.network === network)[0].url.split('?')[0] + '?ref_src=twsrc%5Etfw'
+          return ''
+      }
     }
   }
 </script>
