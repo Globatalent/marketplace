c843a0443fa1eefbdf0a57bc824dcc16c7c2d322
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-02-16 14:59:21 +0100

    Update utils.py

--- marketplace/core/api/utils.py
+++ marketplace/core/api/utils.py
@@ -6,6 +6,6 @@ from rest_framework.views import exception_handler
 
 
 class StandardPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 25
     page_size_query_param = "page_size"
     max_page_size = 300
