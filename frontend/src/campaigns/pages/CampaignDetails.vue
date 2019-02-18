085679dcf22307c5ecd7b89666489f32bbf409d0
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-02-18 09:38:02 +0100

    more fixes

--- frontend/src/campaigns/pages/CampaignDetails.vue
+++ frontend/src/campaigns/pages/CampaignDetails.vue
@@ -268,12 +268,12 @@
             <el-col :xs="24" :md="8" class="campaignDetails-infoContainer-data-title text-right">&nbsp;</el-col>
             <el-col :xs="24" :md="16" id="buyTokenSection"
                     class="campaignDetails-infoContainer-data-text is-last-block">
-              <el-button type="primary" class="is-full-width buyTokensButton" size="big" @click="goToInvest(campaign)"
-                        v-if="hasStarted">
+              <template v-if="hasStarted">
+              <el-button type="primary" class="is-full-width buyTokensButton" size="big" @click="goToInvest(campaign)">
                 {{$tc('message.BuyTokens')}}
               </el-button>
-              <el-button type="primary" class="is-full-width buyTokensButton" disabled size="big"
-                        @click="goToInvest(campaign)" v-else>
+              </template>
+              <el-button type="primary" class="is-full-width buyTokensButton" disabled size="big" v-else>
                 {{$tc('message.ComingSoon')}}
               </el-button>
               <div class="campaignDetails-infoContainer-campaignCopyright">
@@ -344,6 +344,15 @@
       hasStarted () {
         return this.campaign.started < new Date()
       },
+      isVerified () {
+        if (!!this.user) {
+          return false
+        }
+        else {
+          return true
+        }
+
+      }
       twitterUrl () {
         this.$forceUpdate();
         return (
