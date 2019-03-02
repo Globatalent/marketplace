b66fafec87452015db1a4c3dcd70548e213d49fc
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-03-02 15:59:31 +0100

    Update CampaignDetails.vue

--- frontend/src/campaigns/pages/CampaignDetails.vue
+++ frontend/src/campaigns/pages/CampaignDetails.vue
@@ -566,7 +566,7 @@
       },
       collected () {
         if (!!this.token) {
-          return ((this.token.amount - this.token.remaining) * this.token.unit_price).toFixed(2)
+          return (this.token.amount - this.token.remaining) * this.token.unit_price
         }
         return 0
       },
