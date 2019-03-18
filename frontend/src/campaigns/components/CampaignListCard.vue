d726a17ed33676bdacec056c98df6c17e74e074d
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-03-18 14:17:10 +0100

    Update CampaignListCard.vue

--- frontend/src/campaigns/components/CampaignListCard.vue
+++ frontend/src/campaigns/components/CampaignListCard.vue
@@ -27,7 +27,7 @@
             <span class="campaign-name" v-else-if="campaign.id == 4 && locale == 'es-ES'">{{getDescription(zentro.description)}}</span>
             <span class="campaign-name" v-else-if="campaign.id == 126 && locale == 'es-ES'">{{getDescription(vega.description)}}</span>
             <span class="campaign-name" v-else>{{getDescription(campaign.description)}}</span>
-              <a class="show-more-msg" @click="showMore()" v-if="campaign.description && this.campaign.description.length > 50"> {{showMoreMsg()}}</a>
+              <a class="show-more-msg" @click="showMore()" v-if="campaign.description && campaign.description.length > 50"> {{showMoreMsg()}}</a>
             </div>
           </el-col>
         </el-row>
